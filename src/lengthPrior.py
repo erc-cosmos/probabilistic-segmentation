daf98bb2a2a6e0b50ad0b031db396295037f2c66
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-04-10 15:29:54 +0200

    New empirical prior and tests for priors

--- src/lengthPrior.py
+++ src/lengthPrior.py
@@ -1,23 +1,41 @@
-#!/usr/bin/python3
-
+"""Module for arc length priors."""
 import scipy as sp
 import scipy.stats
+import functools
+import collections
+
+
+class ImpossibleCondition(ZeroDivisionError):
+    """Exception raised when conditionning by an impossible event."""
+
+    pass
 
 
 class NormalLengthPrior:
+    """Prior using a truncated normal distribution."""
+
     def __init__(self, mean, stddev, x, maxLength):
+        """Construct a NormalLengthPrior."""
+        if maxLength <= 0:
+            raise ValueError("Positive max length required")
         self.mean = mean
         self.stddev = stddev
         self.x = x
         self.max = maxLength
 
+    def __repr__(self):
+        """Return a human-readable representation of a normal prior."""
+        return f"NormalLengthPrior N({self.mean},{self.stddev}), max={self.max} on {len(self.x)} points"
+
     def evalCond(self, N, i, j):
-        """ Returns the likelihood of a boundary at j given a boundary at i
-        N -- not used anymore TODO: Remove
-        i -- known boundary location
-        j -- examined location
         """
-        if j == i or j >= len(self.x):
+        Return the likelihood of the next boundary being at j given a boundary at i.
+
+        N -- not used anymore TODO: Remove
+        i -- known boundary index
+        j -- examined index
+        """
+        if j <= i or j >= len(self.x):
             return 0
         x0 = self.x[i]
         xmax = min(self.x[-1]-x0, self.max)
@@ -35,21 +53,103 @@ class NormalLengthPrior:
         # Scaling factor for the part of the distribution outside possible values
         scaling = cmax - c0
         if scaling == 0:
-            print('ohoho')
-            return 1
+            raise ImpossibleCondition("Conditioning on impossible event")
         return (cj-cjpred)/scaling
 
     def getMaxIndex(self, i):
-        """ Returns the maximum arc end with non-null prior for an arc starting at i."""
+        """Return the maximum arc end with non-null prior for an arc starting at i."""
         xmax = self.x[i] + self.max
         imax = i + next((it for it, x in enumerate(self.x[i:]) if x > xmax),
                         len(self.x[i:])-1)
         return imax
 
     def getMinIndex(self, j):
-        """ Returns the minimum arc start with non-null prior for an arc ending at i."""
+        """Return the minimum arc start with non-null prior for an arc ending at i."""
         xmin = self.x[j] - self.max
         imin = next((it-1 for it, x in enumerate(self.x[:j]) if x > xmin), 0)
         if imin == -1:
             imin = 0
         return imin
+
+
+class DiscreteLengthPrior:
+    """Prior using a discrete distribution."""
+
+    def __init__(self, dataLength, distribution, maxLength=None):
+        """Construct a DiscreteLengthPrior."""
+        self._dataLength = dataLength
+        self._distrib = distribution
+        if maxLength is None:
+            self._maxLength = max(self.distrib)
+
+    def __repr__(self):
+        """Return a human-readable representation of an empirical prior."""
+        return f"Discrete length prior (max {self._maxLength}): {self._distrib}"
+
+    @property
+    def distrib(self):
+        """Access distrib."""
+        return self._distrib
+
+    @property
+    def dataLength(self):
+        """Access dataLength."""
+        return self._dataLength
+
+    @property
+    def maxLength(self):
+        """Access maxLength."""
+        return self._maxLength
+
+    def evalCond(self, i, j):
+        """
+        Return the likelihood of the next boundary being at j given a boundary at i.
+
+        i -- known boundary index
+        j -- examined index
+        """
+        if j > self.dataLength or j <= i:
+            return 0
+        length = j-i
+        if self.dataLength - i > self.maxLength:
+            scaling = 1
+        else:
+            scaling = self.scalingFactor(i)
+        return self.distrib.get(length, 0)/scaling
+
+    def scalingFactor(self, i):
+        """Return the scaling factor for truncating the underlying the distribution."""
+        result = sum(self.distrib.get(k, 0) for k in range(self.dataLength-i))
+        if result == 0:
+            raise ImpossibleCondition("Conditioning on impossible event")
+        return result
+
+
+class EmpiricalLengthPrior(DiscreteLengthPrior):
+    """Prior using an empirical distribution."""
+
+    def __init__(self, data, dataLength):
+        """Construct an EmpiricalLengthPrior."""
+        self._data = data
+        distribution = inferDiscreteDistribution(data)
+        super().__init__(dataLength, distribution)
+
+    def __repr__(self):
+        """Return a human-readable representation of an empirical prior."""
+        return super().__repr__()+f" based on {len(self._data)} samples"
+
+    def buildDistribution(self, data):
+        """Set distrib from the provided data."""
+        counts = collections.Counter(data)
+        for key in counts:
+            counts[key] /= float(len(data))
+        self._maxLength = max(counts)
+        self._distrib = counts
+
+
+def inferDiscreteDistribution(data, *, reserve=0, domain=None):
+    """Infer a distribution from data."""
+    reserveBoost = reserve/len(domain) if reserve != 0 else 0
+
+    counts = collections.Counter(data)
+    return {x: (1-reserve) * p / len(data) + reserveBoost for x, p in counts.items()}
---
+++ tests/test_lengthPriors.py
@@ -0,0 +1,73 @@
+"""Tests for the classes in lengthPriors."""
+from context import *
+
+import lengthPrior
+
+means = st.floats(min_value=0, allow_infinity=False)
+stddevs = st.floats(min_value=1, exclude_min=True, allow_infinity=False)
+xinputs = st.lists(st.floats(min_value=0), min_size=2, unique=True).map(sorted)
+normalPriors = st.builds(lengthPrior.NormalLengthPrior,
+                         means, stddevs, xinputs,
+                         st.integers(min_value=2, max_value=50))
+segmentLengthSets = st.lists(st.integers(
+    min_value=1, max_value=100), min_size=2)
+pieceLengths = st.integers(min_value=2, max_value=10000)
+empiricalPriors = st.builds(lengthPrior.EmpiricalLengthPrior,
+                            dataLength=pieceLengths,
+                            data=segmentLengthSets)
+
+
+@hypothesis.given(normalPriors,
+                  st.integers(min_value=0),
+                  st.integers(min_value=0))
+def test_proba_Normal(distribution, i, j):
+    """Check that evalCond returns probability-like values."""
+    result = distribution.evalCond(None, i, j)
+    assert 0 <= result <= 1
+
+
+@hypothesis.given(normalPriors,
+                  st.integers(min_value=0))
+def test_proba_sum_Normal_linear(distribution, i):
+    """Check that evalCond sums to 1 over j (if i is possible)."""
+    try:
+        result = sum(distribution.evalCond(None, i, i+j+1)
+                     for j in range(len(distribution.x)))
+        assert result == pytest.approx(1, abs=1e-10) or result == 0
+    except lengthPrior.ImpossibleCondition:
+        pass
+
+
+@hypothesis.given(empiricalPriors,
+                  st.integers(min_value=0))
+def test_proba_sum_Empirical_linear(distribution, i):
+    """Check that evalCond sums to 1 over j (if i is possible)."""
+    try:
+        result = sum(distribution.evalCond(i, j)
+                     for j in range(distribution.dataLength))
+        assert result == pytest.approx(1, abs=1e-10) or result == 0
+    except lengthPrior.ImpossibleCondition:
+        # TODO: Check that it is indeed an impossible condition
+        pass
+
+
+@hypothesis.given(segmentLengthSets, pieceLengths)
+def test_build_Empirical(lengths, dataLength):
+    """Check that EmpiricalPriors get properly constructed from data."""
+    assert lengthPrior.EmpiricalLengthPrior(
+        dataLength=dataLength, data=lengths)
+
+
+def test_empirical_distribution_by_value():
+    """Check a distribution after construction."""
+    dist = lengthPrior.inferDiscreteDistribution([5, 5, 3, 8, 4])
+    assert dist[5] == 0.4
+    assert dist[4] == 0.2
+
+
+@hypothesis.given(segmentLengthSets)
+def test_empirical_distribution_is_distribution(segmentLengthSet):
+    """Check that the inferred distribution is a distribution."""
+    dist = lengthPrior.inferDiscreteDistribution(segmentLengthSet)
+    assert all(0 <= p <= 1 for p in dist.values())
+    assert sum(dist.values()) == pytest.approx(1)
