e5f9733869146ef655bb32d4c649c3dcbd169a5a
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-08-09 13:41:59 +0200

    Formatting and docstrings

--- src/convert.py
+++ src/convert.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python3
+"""Script to convert data from the MazurkaBL format to the CosmoNote format."""
 
-from readers import *
+import readers
 import os
 import csv
 
-tempoData = readAllMazurkaTimings()
-dynData = readAllMazurkaData()
-timingsData = readAllMazurkaData(dirpath="beat_time")
-
+tempoData = readers.readAllMazurkaTimings()
+dynData = readers.readAllMazurkaData()
+timingsData = readers.readAllMazurkaData(dirpath="data/beat_time")
 
 
 data = []
@@ -20,21 +19,21 @@ for (piece, it) in tempoData:
                     for (piece3, it3) in timingsData:
                         for (interpret3, tim) in it3:
                             if interpret == interpret3:
-                                data.append((piece,interpret,tim,[float('NaN'), *tempo],dyn))
+                                data.append((piece, interpret, tim, [float('NaN'), *tempo], dyn))
 
 
-for (piece,performer,timing,tempo,dyn) in data:
+for (piece, performer, timing, tempo, dyn) in data:
     fileBase = f"Chopin_Mazurka-{piece}_{performer}"
-    os.makedirs(os.path.join("CosmoNoteData", fileBase),exist_ok=True)
-    with open(os.path.join("CosmoNoteData", fileBase,fileBase+"_data.csv"),'w') as metaFile :
+    os.makedirs(os.path.join("CosmoNoteData", fileBase), exist_ok=True)
+    with open(os.path.join("CosmoNoteData", fileBase, fileBase+"_data.csv"), 'w') as metaFile:
         csvWriter = csv.writer(metaFile)
-        csvWriter.writerow(["File name","Composer","Piece","Performer"])
-        csvWriter.writerow([fileBase,"Frederic Chopin",f"Mazurka {piece}",performer])
-    with open(os.path.join("CosmoNoteData", fileBase,fileBase+"_tempo.csv"),'w') as tempoFile:
+        csvWriter.writerow(["File name", "Composer", "Piece", "Performer"])
+        csvWriter.writerow([fileBase, "Frederic Chopin", f"Mazurka {piece}", performer])
+    with open(os.path.join("CosmoNoteData", fileBase, fileBase+"_tempo.csv"), 'w') as tempoFile:
         csvWriter = csv.writer(tempoFile)
-        csvWriter.writerow(["Time","Tempo"])
-        csvWriter.writerows(zip(timing,tempo))
-    with open(os.path.join("CosmoNoteData", fileBase,fileBase+"_loudness.csv"),'w') as dynFile:
+        csvWriter.writerow(["Time", "Tempo"])
+        csvWriter.writerows(zip(timing, tempo))
+    with open(os.path.join("CosmoNoteData", fileBase, fileBase+"_loudness.csv"), 'w') as dynFile:
         csvWriter = csv.writer(dynFile)
-        csvWriter.writerow(["Time","Loudness"])
-        csvWriter.writerows(zip(timing,dyn))
+        csvWriter.writerow(["Time", "Loudness"])
+        csvWriter.writerows(zip(timing, dyn))
--- src/defaultPriors.py
+++ src/defaultPriors.py
@@ -1,7 +1,7 @@
-"""Set of default priors, tuned from a small sample."""
+"""Set of default priors, tuned from a small sample of MazurkaBL performances."""
 
 # Set for tempo
-arcPriorTempo = { # Set after first 10 each of M06-1 and M06-2
+arcPriorTempo = {  # Set after first 10 each of M06-1 and M06-2
     # Gaussian priors on the parameters of ax^2 + bx + c
     'aMean': -181,
     'aStd': 93,
@@ -12,13 +12,13 @@ arcPriorTempo = { # Set after first 10 each of M06-1 and M06-2
     'noiseStd': 18.1
 }
 lengthPriorParamsTempo = {
-    'mean':14.7,
-    'stddev':5.95,
-    'maxLength':30
+    'mean': 14.7,
+    'stddev': 5.95,
+    'maxLength': 30
 }
 
 # Set for loudness
-arcPriorLoud = { # Set after first 10 each of M06-1 and M06-2
+arcPriorLoud = {  # Set after first 10 each of M06-1 and M06-2
     # Gaussian priors on the parameters of ax^2 + bx + c
     'aMean': -0.73,
     'aStd': 0.55,
@@ -29,7 +29,7 @@ arcPriorLoud = { # Set after first 10 each of M06-1 and M06-2
     'noiseStd': 0.039
 }
 lengthPriorParamsLoud = {
-    'mean':11.8,
-    'stddev':5.53,
-    'maxLength':30
+    'mean': 11.8,
+    'stddev': 5.53,
+    'maxLength': 30
 }
