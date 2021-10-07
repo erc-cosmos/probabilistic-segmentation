2e5c5ceff5d31d26e455c75778fbd0f061c96e2b
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-10-07 14:35:55 +0200

    Linting

--- src/mydecorators.py
+++ src/mydecorators.py
@@ -3,12 +3,13 @@
 import functools
 import inspect
 
-def preprocess(kw,preprocessor):
+
+def preprocess(kw, preprocessor):
     """Decorate to preprocess a given argument."""
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(*args,**kwargs):
-            kwargs = inspect.getcallargs(func,*args,**kwargs)
+        def wrapper(*args, **kwargs):
+            kwargs = inspect.getcallargs(func, *args, **kwargs)
             kwargs[kw] = preprocessor(kwargs[kw])
             return func(**kwargs)
         return wrapper
@@ -22,14 +23,14 @@ def singleOrList(func=None, *, kw=None):
     If kw is specified, conversion is applied to this argument, otherwise to the first argument
     """
     if func is None:
-        return functools.partial(singleOrList,kw=kw)
+        return functools.partial(singleOrList, kw=kw)
 
     def ensureList(maybeListArg):
         return [maybeListArg] if not isinstance(maybeListArg, list) else maybeListArg
     if kw is None:
         @functools.wraps(func)
-        def wrapper(maybeListArg,*args,**kwargs):
-            return func(ensureList(maybeListArg),*args,**kwargs)
+        def wrapper(maybeListArg, *args, **kwargs):
+            return func(ensureList(maybeListArg), *args, **kwargs)
         return wrapper
     else:
-        return preprocess(kw,ensureList)(func)
+        return preprocess(kw, ensureList)(func)
