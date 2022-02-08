39c1c9aac75d8a46eef70cf49c9c058c0870e1c7
Corentin Guichaoua <guichaoua@math.unistra.fr>
2022-02-08 19:05:01 +0100

    Improvment on visualisations

--- bayes_arcs/segment_viz.py
+++ bayes_arcs/segment_viz.py
@@ -11,6 +11,7 @@ def plot_segment_matrix(data, smoothing):
     # Force the diagonal
     for i in range(np.shape(smoothed)[0]):
         smoothed[i, i] = 1
+
     fig, ax = plt.subplots(figsize=(12, 8), dpi=100)
 
     im = ax.imshow(smoothed, cmap=plt.get_cmap('hot'))
@@ -23,27 +24,28 @@ def plot_segment_matrix(data, smoothing):
     fig.show()
 
 
-def plot_segment_raindrop(data, max_length):
+def plot_segment_raindrop(data, max_length, ratio=4, show=True):
     """Display a raindrop type graph for segmentation."""
     linear_viz_point = np.zeros((np.shape(data)[0], max_length))
     for i in range(np.shape(data)[0]):
         for j in range(max_length):
             if i+j < np.shape(data)[0]:
                 linear_viz_point[i, j] = linear_viz_point[i, j] + data[i, i+j]
-    fig, ax = plt.subplots(figsize=(16, 4), dpi=100)
+    height = 4
+    fig, ax = plt.subplots(figsize=(ratio*height, height), dpi=100)
 
-    im = ax.imshow(np.transpose(linear_viz_point), cmap=plt.get_cmap('hot'), origin='lower', aspect=4)
+    im = ax.imshow(np.transpose(linear_viz_point), cmap=plt.get_cmap('hot'), origin='lower', aspect=ratio)
     cbar = fig.colorbar(im)
     cbar.set_label("Likelihood")
     ax.set_ylabel('Duration')
     ax.set_xlabel('Start')
     ax.set_title('Posterior likelihood of segment')
-
-    fig.show()
+    if show:
+        fig.show()
     return fig
 
 
-def plot_segment_beams(post2_bidim, max_length):
+def plot_segment_beams(post2_bidim, max_length, ratio=4, show=True):
     """Display a beams-like graph for segmentation."""
     linear_viz = np.zeros((np.shape(post2_bidim)[0], max_length))
     for i in range(np.shape(post2_bidim)[0]):
@@ -51,16 +53,17 @@ def plot_segment_beams(post2_bidim, max_length):
             if i+j < np.shape(post2_bidim)[0]:
                 for k in range(j):
                     linear_viz[i+k, j] = linear_viz[i+k, j] + post2_bidim[i, i+j]
-    fig, ax = plt.subplots(figsize=(16, 4), dpi=100)
+    height = 4
+    fig, ax = plt.subplots(figsize=(ratio*height, height), dpi=100)
 
-    im = ax.imshow(np.transpose(linear_viz), cmap=plt.get_cmap('hot'), origin='lower', aspect=4)
+    im = ax.imshow(np.transpose(linear_viz), cmap=plt.get_cmap('hot'), origin='lower', aspect=ratio)
     cbar = fig.colorbar(im)
     cbar.set_label("Likelihood")
     ax.set_ylabel('Segment Duration')
     ax.set_xlabel('Point position')
     ax.set_title('Posterior likelihood of point within segment')
-
-    fig.show()
+    if show:
+        fig.show()
     return fig
 
 
@@ -70,7 +73,7 @@ def plot_segment_with_signal(post_marginals, data, data_time, boundaries=None, *
     """Display boundary marginals with the input data."""
     fig, ax1 = plt.subplots(figsize=(12, 8), dpi=100)
 
-    ax1.plot(data_time, data, color=data_color)
+    line_data = ax1.plot(data_time, data, color=data_color)
     ax2 = plt.twinx()
 
     if boundaries is not None:
@@ -78,14 +81,16 @@ def plot_segment_with_signal(post_marginals, data, data_time, boundaries=None, *
 
     post2_smoothed = np.convolve(np.ones(smoothing), post_marginals, mode='same')
 
-    ax2.plot(data_time, post2_smoothed, color=post_color)
+    line_proba = ax2.plot(data_time, post2_smoothed, color=post_color)
 
     ax1.set_ylabel(input_label)
-    ax2.set_xlabel(time_label)
+    ax1.set_xlabel(time_label)
     ax2.set_ylabel('Likelihood')
     ax2.set_ylim((0, 1))
     ax2.set_title('Posterior likelihood of boundaries')
-
+    lines = line_data + line_proba
+    labels = [input_label, 'likelihood of boundary']
+    ax1.legend(lines, labels)
     if show:
         fig.show()
     return fig
